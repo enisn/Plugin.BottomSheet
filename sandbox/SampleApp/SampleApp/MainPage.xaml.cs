3b71ff1c05fcae47733f473a3331a8bd1b91ffbd
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-09-22 00:50:20 +0300

    Move all logic to library

--- sandbox/SampleApp/SampleApp/MainPage.xaml.cs
+++ sandbox/SampleApp/SampleApp/MainPage.xaml.cs
@@ -19,42 +19,42 @@ namespace SampleApp
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            bottomSheet.TranslateTo(0, 0, 250);
+            //bottomSheet.TranslateTo(0, 0, 250);
         }
 
         private void Button_ToTop_Clicked(object sender, EventArgs e)
         {
-            bottomSheet.TranslateTo(bottomSheet.X, -(Height * 0.85));
+            //bottomSheet.TranslateTo(bottomSheet.X, -(Height * 0.85));
         }
 
-        void OnPanUpdated(object sender, PanUpdatedEventArgs e)
-        {
-            var translateY = Math.Max(Math.Min(0, -bottomSheet.TranslationY + e.TotalY), -Math.Abs((Height * 0.1) - Height));
-            // Handle the pan
-            switch (e.StatusType)
-            {
-                case GestureStatus.Running:
-                    bottomSheet.TranslateTo(bottomSheet.X, bottomSheet.TranslationY + e.TotalY, 50);
-                    break;
-                case GestureStatus.Completed:
-                case GestureStatus.Canceled:
-                    y = bottomSheet.TranslationY;
+        //void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        //{
+        //    var translateY = Math.Max(Math.Min(0, -bottomSheet.TranslationY + e.TotalY), -Math.Abs((Height * 0.1) - Height));
+        //    // Handle the pan
+        //    switch (e.StatusType)
+        //    {
+        //        case GestureStatus.Running:
+        //            bottomSheet.TranslateTo(bottomSheet.X, bottomSheet.TranslationY + e.TotalY, 50);
+        //            break;
+        //        case GestureStatus.Completed:
+        //        case GestureStatus.Canceled:
+        //            y = bottomSheet.TranslationY;
 
-                    Console.WriteLine("TranslationY: ["+ Math.Abs(bottomSheet.TranslationY) + " ]");
-                    if (Math.Abs(bottomSheet.TranslationY) > Height * .2 )
-                    {
-                        bottomSheet.TranslateTo(bottomSheet.X, -(Height * 0.85));
-                    }
-                    else
-                    {
-                    //bottomSheet.TranslateTo(bottomSheet.X, translateY, 50);
-                        bottomSheet.TranslateTo(bottomSheet.X, 0);
-                    }
+        //            Console.WriteLine("TranslationY: ["+ Math.Abs(bottomSheet.TranslationY) + " ]");
+        //            if (Math.Abs(bottomSheet.TranslationY) > Height * .2 )
+        //            {
+        //                bottomSheet.TranslateTo(bottomSheet.X, -(Height * 0.85));
+        //            }
+        //            else
+        //            {
+        //            //bottomSheet.TranslateTo(bottomSheet.X, translateY, 50);
+        //                bottomSheet.TranslateTo(bottomSheet.X, 0);
+        //            }
 
-                    break;
+        //            break;
 
-            }
+        //    }
 
-        }
+        //}
     }
 }
--- src/Plugin.BottomSheet/BottomSheetLayout.cs
+++ src/Plugin.BottomSheet/BottomSheetLayout.cs
@@ -1,10 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Plugin.BottomSheet
 {
-    public class BottomSheetLayout
+    [ContentProperty(nameof(Content))]
+    public class BottomSheetLayout : RelativeLayout
     {
+        private View bottomSheetContent;
+        private readonly Frame bottomFrame;
+        private readonly StackLayout bottomContainer = new StackLayout { };
+        private readonly ContentView pageContainer = new ContentView { Padding = 0, Margin = 0 };
+        public BottomSheetLayout()
+        {
+            bottomFrame = new Frame
+            {
+                CornerRadius = 8,
+                HasShadow = true,
+                Padding = 5,
+                Content = bottomContainer
+            };
+            var recognizer = new PanGestureRecognizer();
+            recognizer.PanUpdated += Recognizer_PanUpdated;
+            bottomFrame.GestureRecognizers.Add(recognizer);
+
+            this.Children.Add(pageContainer, xConstraint: null, yConstraint: null, null, null);
+            this.Children.Add(bottomFrame,
+                yConstraint: Constraint.RelativeToParent(p => p.Height * .95),
+                widthConstraint: Constraint.RelativeToParent(p => p.Width),
+                heightConstraint: Constraint.RelativeToParent(p => p.Height * .95)
+                );
+        }
+
+        public View Content { get => pageContainer.Content; set => pageContainer.Content = value; }
+        public View BottomSheetContent
+        {
+            get => bottomSheetContent;
+            set { bottomSheetContent = value; InitBottomSheet(); }
+        }
+
+        public virtual View GenerateAnchor()
+        {
+            return new BoxView
+            {
+                HeightRequest = 5,
+                CornerRadius = 2,
+                WidthRequest = 50,
+                BackgroundColor = Color.Gray,
+                HorizontalOptions = LayoutOptions.Center
+            };
+        }
+
+        private void InitBottomSheet()
+        {
+            bottomContainer.Children.Clear();
+            bottomContainer.Children.Add(GenerateAnchor());
+            bottomContainer.Children.Add(BottomSheetContent);
+            bottomFrame.BackgroundColor = BottomSheetContent.BackgroundColor;
+        }
+
+        private void Recognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    bottomFrame.TranslateTo(bottomFrame.X, bottomFrame.TranslationY + e.TotalY, 50);
+                    break;
+                case GestureStatus.Completed:
+                case GestureStatus.Canceled:
+                    if (Math.Abs(bottomFrame.TranslationY) > Height * .2)
+                    {
+                        bottomFrame.TranslateTo(bottomFrame.X, -(Height * 0.90));
+                    }
+                    else
+                    {
+                        bottomFrame.TranslateTo(bottomFrame.X, 0);
+                    }
+                    break;
+            }
+        }
     }
 }
